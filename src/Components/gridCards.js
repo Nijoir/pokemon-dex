a0f2718d170357d3b4c2d6c2846c5cea4040e981
Nijoir <nicolazeti@hotmail.com>
2022-05-09 05:25:55 -0400

    more visuals for cards

--- src/Components/gridCards.js
+++ src/Components/gridCards.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import {  Divider } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,19 +16,36 @@ const Item = styled(Paper)(({ theme }) => ({
   
 
 export default function GridCards({ pokemon, id}) {
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1).toLowerCase();
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1).toLowerCase()
+
+    
     return (
         <Grid key={id} item xs={6}>
             <Item>
-              <Grid item xs="auto">
-                <h2>{name}</h2>
-                <img alt={name} src={pokemon.sprites.front_default}></img>
-              </Grid>
-              <Grid item xs="auto">
-                  <p><b>HP:</b> {pokemon.stats[0].base_stat} <b>Ataque:</b> {pokemon.stats[1].base_stat}</p>
-                  <p><b>Defensa:</b> {pokemon.stats[2].base_stat} <b>At.Especial:</b> {pokemon.stats[3].base_stat}</p>
-                  <p><b>Df.Especial:</b> {pokemon.stats[4].base_stat} <b>Velocidad:</b> {pokemon.stats[5].base_stat}</p>
-              </Grid>
+                <a>Nro: </a>
+                <span>{`${pokemon.id}`}</span>
+                <Divider></Divider>
+                <Grid item xs="auto">
+                    <h2>{name}</h2>
+                    <img alt={name} src={pokemon.sprites.front_default}></img>
+                </Grid>
+                <a>Type:</a>
+                {pokemon.types.map((types, id) => {
+                    return (
+                        <span key={id}>{types.type.name}</span>
+                    )
+                })}
+                <Divider></Divider>
+                { pokemon.stats.map( (stats, id) => {
+                    const statName = stats.stat.name.toUpperCase()
+                    return (
+                        <Grid item xs="auto">
+                            <a>{`${stats.stat.name}: `}</a>
+                            <span>{`${stats.base_stat}`}</span>
+                        </Grid>
+                    )
+                })
+                }
             </Item>
         </Grid>
     )
