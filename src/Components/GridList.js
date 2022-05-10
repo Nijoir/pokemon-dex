680aa6f4f5bb6084b9b1a1bf31ee233532e2d5bc
Nijoir <nicolazeti@hotmail.com>
2022-05-10 16:13:59 -0400

    Update GridList.js

--- src/Components/GridList.js
+++ src/Components/GridList.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import { Grid, Pagination, Typography, Divider } from "@mui/material";
 
-import { Grid, Pagination, Typography } from "@mui/material";
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    margin: '10px',
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 
 
